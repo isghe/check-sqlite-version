d376a14eae9a6c3b639b2f8a89a1e919060afd6b
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2019-05-04 16:56:23 +0200

    returns EXIT_SUCCESS or EXIT_FAILURE

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h>
+#include <string.h>
 
 #ifdef DEBUG
-#include <string.h>
 #include <assert.h>
 #endif
 
 int main(void){
+	int ret = EXIT_FAILURE;
+
 	printf ("SQLITE_VERSION_NUMBER: %u;\n", SQLITE_VERSION_NUMBER);
 	printf ("sqlite3_libversion_number(): %u;\n", sqlite3_libversion_number());
 
@@ -19,7 +21,15 @@ int main(void){
 	assert( sqlite3_libversion_number()==SQLITE_VERSION_NUMBER );
 	assert( strncmp(sqlite3_sourceid(),SQLITE_SOURCE_ID,80)==0 );
 	assert( strcmp(sqlite3_libversion(),SQLITE_VERSION)==0 );
+	ret = EXIT_SUCCESS;
+	#else
+	if ((sqlite3_libversion_number()==SQLITE_VERSION_NUMBER)
+		&& (strncmp(sqlite3_sourceid(),SQLITE_SOURCE_ID,80)==0)
+		&& (strcmp(sqlite3_libversion(),SQLITE_VERSION)==0)){
+		ret = EXIT_SUCCESS;
+	}
+	printf ("ret: %d;\n", ret);
 	#endif
 
-	return EXIT_SUCCESS;
+	return ret;
 }
