d9a630cbff9bb4860d14021fea0435baa80b0d8e
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2019-05-04 16:30:00 +0200

    asserts in DEBUG version

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ EXECUTABLE = out/check-sqlite-version
 all: release debug
 
 debug: Makefile main.c
-	$(CC) $(CFLAGS) -g -D_DEBUG main.c -lsqlite3 -o $(EXECUTABLE)_d.out
+	$(CC) $(CFLAGS) -g -DDEBUG main.c -lsqlite3 -o $(EXECUTABLE)_d.out
 
 release: Makefile main.c
 	$(CC) $(CFLAGS) -O3 main.c -lsqlite3 -o $(EXECUTABLE).out
--- main.c
+++ main.c
@@ -2,8 +2,24 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 
+#ifdef DEBUG
+#include <string.h>
+#include <assert.h>
+#endif
+
 int main(void){
 	printf ("SQLITE_VERSION_NUMBER: %u;\n", SQLITE_VERSION_NUMBER);
 	printf ("sqlite3_libversion_number(): %u;\n", sqlite3_libversion_number());
+
+	#ifdef DEBUG
+	/*
+	* https://www.sqlite.org/c3ref/libversion.html
+	*/
+
+	assert( sqlite3_libversion_number()==SQLITE_VERSION_NUMBER );
+	assert( strncmp(sqlite3_sourceid(),SQLITE_SOURCE_ID,80)==0 );
+	assert( strcmp(sqlite3_libversion(),SQLITE_VERSION)==0 );
+	#endif
+
 	return EXIT_SUCCESS;
 }
